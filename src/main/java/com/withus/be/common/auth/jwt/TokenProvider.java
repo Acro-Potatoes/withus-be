8c3fcb23e76b217752a3f81ebf433dc6c1528ab3
leewg97 <leewg97@naver.com>
2024-01-25 16:26:08 +0900

    Feat #11 : redis 설정

--- src/main/java/com/withus/be/common/auth/jwt/TokenProvider.java
+++ src/main/java/com/withus/be/common/auth/jwt/TokenProvider.java
@@ -11,6 +11,8 @@ import io.jsonwebtoken.security.SecurityException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
