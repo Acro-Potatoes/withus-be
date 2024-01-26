2f5d5dd1c91ddba6c006073fd2e907a6b6541ef8
leewg97 <leewg97@naver.com>
2024-01-26 12:36:30 +0900

    Refactor : 설정파일 수정

--- src/main/java/com/withus/be/config/SecurityConfig.java
+++ src/main/java/com/withus/be/config/SecurityConfig.java
@@ -4,7 +4,7 @@ import com.withus.be.common.auth.jwt.JwtAccessDeniedHandler;
 import com.withus.be.common.auth.jwt.JwtAuthenticationEntryPoint;
 import com.withus.be.common.auth.jwt.JwtSecurityConfig;
 import com.withus.be.common.auth.jwt.TokenProvider;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -20,6 +20,7 @@ import org.springframework.web.filter.CorsFilter;
 @EnableWebSecurity
 @EnableMethodSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
     private final TokenProvider tokenProvider;
     private final CorsFilter corsFilter;
@@ -27,55 +28,37 @@ public class SecurityConfig {
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     private static final String[] WHITE_LIST_URL = {
-            "/", "/**", "/v2/api-docs", "/swagger-resources/**",
-            "/swagger-ui.html", "/swagger-ui/index.html", "/webjars/**", "/swagger/**"
+            "/swagger-resources/**", "/swagger-ui.html", "/swagger-ui/index.html", "/webjars/**", "/swagger/**"
     };
-
-    public SecurityConfig(
-            TokenProvider tokenProvider,
-            CorsFilter corsFilter,
-            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
-            JwtAccessDeniedHandler jwtAccessDeniedHandler
-    ) {
-        this.tokenProvider = tokenProvider;
-        this.corsFilter = corsFilter;
-        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
-        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
                 .headers(headerConfig ->
                         headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
                 )
-
                 .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(exceptionHandling -> exceptionHandling
                         .accessDeniedHandler(jwtAccessDeniedHandler)
                         .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 )
-
                 .authorizeHttpRequests(authorizeHttpRequests ->
                         authorizeHttpRequests
                                 .requestMatchers(WHITE_LIST_URL).permitAll()
                                 .requestMatchers("/auth/login", "auth/google/**", "/auth/signup").permitAll()
-                                .requestMatchers(PathRequest.toH2Console()).permitAll()
+//                                .requestMatchers(PathRequest.toH2Console()).permitAll()
                                 .anyRequest().authenticated()
                 )
-
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-
                 .headers(headers ->
                         headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
                 )
-
                 .with(new JwtSecurityConfig(tokenProvider), customizer -> {
                 })
-
                 .build();
     }
 }
