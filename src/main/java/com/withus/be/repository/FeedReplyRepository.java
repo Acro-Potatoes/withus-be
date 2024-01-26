07506325cc9dee575a5e22557fe9c9823cba4fcb
최예진 <bibianabb@naver.com>
2024-01-26 13:14:59 +0900

    Feat #19: Feed 댓글, 좋아요 WIP

---
+++ src/main/java/com/withus/be/repository/FeedReplyRepository.java
@@ -0,0 +1,7 @@
+package com.withus.be.repository;
+
+import com.withus.be.domain.FeedReply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedReplyRepository extends JpaRepository<FeedReply, Long> {
+}
