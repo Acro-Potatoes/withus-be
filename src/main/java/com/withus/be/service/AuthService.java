2b095fb6e7bfa93a9c0130b955467d79031c8c7f
leewg97 <leewg97@naver.com>
2024-01-30 16:50:01 +0900

    Feat : 회원가입시 이미지 없으면 기본 이미지 추가

--- src/main/java/com/withus/be/service/AuthService.java
+++ src/main/java/com/withus/be/service/AuthService.java
@@ -5,6 +5,7 @@ import com.withus.be.dto.MemberDto.MemberRequest;
 import com.withus.be.dto.MemberDto.MemberResponse;
 import com.withus.be.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final MemberRepository memberRepository;
+    @Value("${cloud.aws.s3.default-image}")
+    private String imageUrl;
 
     @Transactional
     public MemberResponse signup(MemberRequest memberRequest) {
@@ -21,7 +24,7 @@ public class AuthService {
             throw new DuplicatedException("이미 가입되어 있는 유저입니다.");
 
         return MemberResponse.of(memberRepository.save(
-                MemberRequest.from(memberRequest, passwordEncoder.encode(memberRequest.getPassword()))));
+                MemberRequest.from(memberRequest, passwordEncoder.encode(memberRequest.getPassword()), imageUrl)));
     }
 
 }
